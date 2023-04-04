a26358bbc2f8e55d05ae5e5d1fbabdf9b6a248d3
Noah Hicks <noahg@ad.unc.edu>
2023-04-04 13:01:21 -0400

    Some API methods

--- server.js
+++ server.js
@@ -8,3 +8,18 @@ var argv = minimist(process.argv.slice(2));
 var app = express()
 
 const port = argv.port || 5000;
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+
+app.get('/app/', (req, res) => {
+    res.status(200).send("200 OK")
+})
+
+app.get('/app/rps/', (req, res) => { 
+    res.status(200).send(rps(null));
+})
+
+app.get('/app/rpsls/', (req, res) => { 
+    res.status(200).send(rpsls(null));
+})
